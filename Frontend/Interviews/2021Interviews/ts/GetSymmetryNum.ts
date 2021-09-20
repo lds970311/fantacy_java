ffc25fb84533246420d6c93578a85c19f42ea3d2
李德晟 <2696612575@qq.com>
2021-09-21 00:50:15 +0800

    1. 面试题: css左右固定宽度, 中间自适应 2. 打印1-10000所有的对称数字

---
+++ Frontend/Interviews/2021Interviews/ts/GetSymmetryNum.ts
@@ -0,0 +1,12 @@
+const getSymmetryNum = function (start: number, end: number): void {
+    start = start <= 11 ? 11 : start;
+    for (let i = start; i < end; i++) {
+        let initial = i + "";
+        let handledStr = initial.split("").reverse().join("")
+        if (initial === handledStr) {
+            console.log(i)
+        }
+    }
+}
+
+getSymmetryNum(1, 10000)
