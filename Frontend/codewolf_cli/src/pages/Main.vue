9b57f3a74070c2648b9dfca60e0b7b6dc1d40126
李德晟 <2696612575@qq.com>
2021-10-16 03:01:30 +0800

    1. 完成脚手架vue模块

---
+++ Frontend/codewolf_cli/src/pages/Main.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  },
+  components: {},
+  mixins: [],
+  data() {
+    return {
+      message: "Main"
+    }
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+.main {
+
+}
+</style>
+
+
