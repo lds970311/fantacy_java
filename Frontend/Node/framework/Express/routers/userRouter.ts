e57c3cded50818d26ffcb5689af17a95a7e92ebf
李德晟 <2696612575@qq.com>
2021-10-27 20:15:29 +0800

    1. Express使用router 2. 初始化Koa

---
+++ Frontend/Node/framework/Express/routers/userRouter.ts
@@ -0,0 +1,29 @@
+import {Router} from "express";
+import * as core from "express-serve-static-core";
+
+// @ts-ignore
+const router: core.Router = new Router({
+    caseSensitive: true,
+    mergeParams: false,
+    strict: true
+})
+
+const userRouter = (): Router => {
+    router.get("/:id", (req, res) => {
+        console.log("111")
+        res.json({
+            code: 200,
+            status: "success"
+        })
+    })
+
+    router.post("/", ((req, res) => {
+        req.on("data", chunk => {
+            console.log(chunk);
+            res.json(chunk.toString())
+        })
+    }))
+    return router
+}
+
+export default userRouter()
