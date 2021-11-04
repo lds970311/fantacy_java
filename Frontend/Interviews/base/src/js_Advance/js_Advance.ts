1c1267481051ae14a8e9cfd680fa8dd9c2d67bab
李德晟 <2696612575@qq.com>
2021-11-04 10:19:42 +0800

    1. 编写事件监听通用函数 2. babel配置

--- Frontend/Interviews/base/src/js_Advance/js_Advance.ts
+++ Frontend/Interviews/base/src/js_Advance/js_Advance.ts
@@ -1,4 +1,12 @@
 import "../../assets/style/common.less"
+import "../../assets/style/js_adv/js_adv.less"
 
 //动态创建标签
 import "../plugins/menuCreator"
+
+//绑定事件
+import "./events/eventBind"
+
+setTimeout(() => {
+    console.log("hello")
+}, 0)
--- Frontend/Interviews/base/webpack.config.ts
+++ Frontend/Interviews/base/webpack.config.ts
@@ -67,7 +67,8 @@ const config = {
                     {
                         loader: "babel-loader",
                     }
-                ]
+                ],
+                exclude: /node_modules/
             }
         ]
     },
