03f372175f6c8bedb40081a05c2e11295888dc79
李德晟 <2696612575@qq.com>
2021-11-22 20:56:25 +0800

    1. react-todolist实现

--- Frontend/Interviews/frameworks/react-interview/src/App.tsx
+++ Frontend/Interviews/frameworks/react-interview/src/App.tsx
@@ -2,6 +2,7 @@ import './App.scss'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import {lazy, Suspense} from "react";
 import Advanced from "./pages/Advanced/Advanced";
+import ToDo from "./pages/Todo/Todo";
 
 const Home = lazy(() => import ("./pages/Home/Home"));
 
@@ -14,6 +15,7 @@ function App() {
                     <Routes>
                         <Route path={"/"} element={<Home/>}/>
                         <Route path={"/advance"} element={<Advanced/>}/>
+                        <Route path="/todo" element={<ToDo/>}/>
                     </Routes>
                 </Suspense>
             </BrowserRouter>
---
+++ Frontend/Interviews/frameworks/react-interview/src/components/ToDo/InputItem.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+
+
+interface State {
+    addItem: (content: string) => void;
+}
+
+const InputItem: React.FunctionComponent<State> = (props: State) => {
+    const [val, setVal] = useState("")
+
+    function clickHandler() {
+        const {addItem} = props
+        if (val.trim() !== "") {
+            addItem(val)
+            setVal("")
+        }
+    }
+
+    function handleInputChange(event: any) {
+        setVal(event.target.value)
+    }
+
+    return (
+        <div className="input-item">
+            <input type="text" value={val} onChange={handleInputChange}
+                   style={{
+                       width: "200px",
+                       marginRight: "15px"
+                   }}
+            />
+            <button onClick={clickHandler}>新增</button>
+        </div>
+    );
+};
+
+export default InputItem;
