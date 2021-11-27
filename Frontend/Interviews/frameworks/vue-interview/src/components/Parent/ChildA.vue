e6617753c9bb7d9dcb7e26a72bc78599ff5ec69a
李德晟 <2696612575@qq.com>
2021-11-27 18:08:52 +0800

    1. children

---
+++ Frontend/Interviews/frameworks/vue-interview/src/components/Parent/ChildA.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <slot name="main"></slot>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: "ChildA"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Frontend/Interviews/frameworks/vue-interview/src/pages/Parent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>parent</h2>
+    <ChildA>
+      <template v-slot:main>
+        <h2>
+          ChildA
+        </h2>
+      </template>
+    </ChildA>
+    <ChildB>
+      <!--      <button @click="getChildren">获取子组件</button>-->
+    </ChildB>
+    <button @click="getChildren">获取子组件</button>
+  </div>
+</template>
+
+<script lang="js">
+import ChildA from "../components/Parent/ChildA.vue";
+import ChildB from "../components/Parent/ChildB.vue";
+
+export default {
+  name: "Parent",
+  components: {
+    ChildA,
+    ChildB
+  },
+  methods: {
+    getChildren() {
+      console.log(1123)
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      console.log(this.$children)
+    }, 0)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
