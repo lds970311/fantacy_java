42f1d2ca6fd30768ffa302d551197eb5719bf9b3
李德晟 <2696612575@qq.com>
2021-11-29 17:54:13 +0800

    1. dart基础语法

--- Frontend/uni-app/second-uni/src/main.js
+++ Frontend/uni-app/second-uni/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App'
+import store from "./store/index.js"
 
 Vue.config.productionTip = false
+Vue.prototype.$store = store
 
 App.mpType = 'app'
 
 const app = new Vue({
-  ...App
+	...App,
 })
 app.$mount()
